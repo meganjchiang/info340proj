e203074d6ab8f09f0f3b57d6f310be8aae21ba03
Sunghee Park <sungheepark@Sunghees-MacBook-Pro.local>
2023-11-22 13:55:05 -0800

    added handleSubmit

--- src/components/CreateAccount.js
+++ src/components/CreateAccount.js
@@ -2,61 +2,76 @@ import React, { useState } from 'react'; //import React Component
 
 function CreateAccountForm() {
 
-    /* const [typedVal, setTypedVal] = useState("");
+    const [firstn, setFirstN] = useState("");
+    const [lastn, setLastN] = useState("");
+    const [gradYear, setGradYear] = useState("");
+    const [major, setMajor] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-    const handleChange = (event) => {
-        const inputValue = event.target.value;
-        setTypedVal(inputValue);
-
-    }
     const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log("submit form with", typedValue);
 
-    } */
+        console.log("Submit form with:");
+        console.log("First Name:", firstn);
+        console.log("Last Name:", lastn);
+        console.log("Graduation Year:", gradYear);
+        console.log("Major:", major);
+        console.log("Email:", email);
+        console.log("Password:", password);
+
+        setFirstN("");
+        setLastN("");
+        setGradYear("");
+        setMajor("");
+        setEmail("");
+        setPassword("");
+    }
+
+
+
     return(
     <form className='application-form'>
         <div className="container">
             <div className='row'>
                 <div className="col">
-                    <label htmlFor="first-name" className="form-label">First Name <span className="required" /* onChange={handleChange} */> *</span></label>
-                    <input type="text" className="form-control" name="first-name" id="first-name" required></input>
+                    <label htmlFor="first-name" className="form-label">First Name <span className="required"> *</span></label>
+                    <input type="text" className="form-control" onChange={(e) => setFirstN(e.target.value)} name="first-name" id="first-name" required></input>
                 </div>
 
                 <div className="col">
-                    <label htmlFor="last-name" className="form-label">Last Name<span className="required" /* onChange={handleChange} */> *</span></label>
-                    <input type="text" className="form-control" name="last-name" id="last-name" required></input>
+                    <label htmlFor="last-name" className="form-label">Last Name<span className="required"> *</span></label>
+                    <input type="text" className="form-control" onChange={(e) => setLastN(e.target.value)} name="last-name" id="last-name" required></input>
                 </div>
             </div>
 
             <div className='row'>
                 <div className="col">
-                    <label htmlFor="grad-year" className="form-label">Intended Graduation Year<span className="required" /* onChange={handleChange} */> *</span></label>
-                    <input type="text" className="form-control" placeholder="example: 2024" name="grad-year" id="grad-year"
+                    <label htmlFor="grad-year" className="form-label">Intended Graduation Year<span className="required"> *</span></label>
+                    <input type="text" className="form-control" onChange={(e) => setGradYear(e.target.value)} placeholder="example: 2024" name="grad-year" id="grad-year"
                         required></input>
                 </div>
 
                 <div className="col">
-                    <label htmlFor="major" className="form-label">Major/Intended Major<span className="required" /* onChange={handleChange} */>
+                    <label htmlFor="major" className="form-label">Major/Intended Major<span className="required">
                         *</span></label>
-                    <input type="text" className="form-control" name="degree" id="degree" required></input>
+                    <input type="text" className="form-control" onChange={(e) => setMajor(e.target.value)} name="degree" id="degree" required></input>
                 </div>
             </div>
 
             <div className="row">
                 <div className="col-12">
-                    <label htmlFor="Email" className="form-label">Email address<span className="required" /* onChange={handleChange} */> *</span></label>
-                    <input type="email" className="form-control" placeholder="name@example.com" name="email" id="email"
+                    <label htmlFor="Email" className="form-label">Email address<span className="required"> *</span></label>
+                    <input type="email" className="form-control" onChange={(e) => setEmail(e.target.value)} placeholder="name@example.com" name="email" id="email"
                         required></input>
                 </div>
             </div>
             <div className="col-7 mb-3">
-                <label htmlFor="password" className="form-label">Password<span className="required" /* onChange={handleChange} */> *</span></label>
-                <input type="password" className="form-control" name="password" id="password" required></input>
+                <label htmlFor="password" className="form-label">Password<span className="required"> *</span></label>
+                <input type="password" className="form-control" onChange={(e) => setPassword(e.target.value)} name="password" id="password" required></input>
             </div>
 
             <div className="col-12 text-center">
-                <button className="submit btn tbn-primary" type="submit">Submit</button>
+                <button className="submit btn tbn-primary" type="submit" onSubmit={handleSubmit} onClick={handleClick}>Submit</button>
             </div>
         </div>
     </form >
