8ca46a456591170dea7ac1f24ee6c255513e068e
Sunghee Park <sungheepark@Sunghees-MacBook-Pro.local>
2023-11-22 12:34:32 -0800

    edited home css

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,6 @@ import React from 'react';
 export function Home() {
   return (
     <div className="splash">
-  
       <main className='home-page'>
         <h1 className="page-title">HUSKY2HUSKY</h1>
         <div className="text-center">
@@ -16,39 +15,40 @@ export function Home() {
           <a href="student_application.html" class="btn btn-light mt-auto">Sign Up</a>
         </div>
       </main>
-    <div className="description">
-      <div className="about-us-section">
-        <div className="row">
-          <div className="col-md-6 mb-4 mb-md-0">
-            <img className="uw-logo" src='img/uw-logo.png' alt="UW logo" />
-          </div>
-    
-          <div className="col-md-6 mt-4 mt-md-0">
-            <h2 className="home-section-title">About us</h2>
-            <p>Husky2Husky aims to provide connections between UW alumni and prospective/current students. Our alumni are here to be mentors, offer advice, and expand students' professional network.</p>
-          </div>
-        </div>
-      </div>
 
-      <hr />
+      <div className="description">
+        <div className="about-us-section">
+          <div className="row">
+            <div className="about-us-img col-md-6 mb-4 mb-md-0">
+              <img className="uw-logo float-start" src='img/uw-logo.png' alt="UW logo" />
+            </div>
 
-      <div className="become-mentor-section">
-        <div className="row">
-          <div className="col-md-6 mb-4 mb-md-0">
-            <h2 className="home-section-title">Become a mentor</h2>
-
-            <p>Apply to be a mentor today to encourage students' growth and give back to the UW community. As a mentor, you'll have the opportunity to share your expertise, provide guidance, and foster a supportive learning environment for students. </p>
-            <p>
-            Join our mentorship program to make a meaningful impact on students' journeys and contribute to the vibrant spirit of collaboration at the University of Washington!
-            </p>
-            <a href="mentor_application.html" class="btn btn-outline-dark mt-auto" id="apply-now">Apply Now</a>
-          </div>
-
-          <div className="mentor-img col-md-6 mt-4 mt-md-0">
-            <img className="mentor-graphic" src='img/mentor_graphic.png' alt="graphic of people helping each other" />
+            <div className="about-us-p col-md-6 mt-4 mt-md-0">
+              <h2 className="home-section-title">About us</h2>
+              <p>Husky2Husky aims to provide connections between UW alumni and prospective/current students. Our alumni are here to be mentors, offer advice, and expand students' professional network.</p>
+            </div>
           </div>
         </div>
-      </div>
+
+        <hr />
+
+        <div className="become-mentor-section">
+          <div className="row">
+            <div className="col-md-6 mb-4 mb-md-0">
+              <h2 className="home-section-title">Become a mentor</h2>
+
+              <p>Apply to be a mentor today to encourage students' growth and give back to the UW community. As a mentor, you'll have the opportunity to share your expertise, provide guidance, and foster a supportive learning environment for students. </p>
+              <p>
+                Join our mentorship program to make a meaningful impact on students' journeys and contribute to the vibrant spirit of collaboration at the University of Washington!
+              </p>
+              <a href="mentor_application.html" class="btn btn-outline-dark mt-auto" id="apply-now">Apply Now</a>
+            </div>
+
+            <div className="mentor-img col-md-6 mt-4 mt-md-0">
+              <img className="mentor-graphic" src='img/mentor_graphic.png' alt="graphic of people helping each other" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
