b6e3be57c63e9c92893dcb085b85a61e308c7c95
Lasya <lasya19@uw.edu>
2023-12-06 21:04:36 -0800

    added simple login

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,38 +1,56 @@
 import React, { useState } from 'react'; //import React Component
 import { Link } from 'react-router-dom';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
+import { getAuth, GoogleAuthProvider, EmailAuthProvider } from 'firebase/auth'
 
-function LoginForm() {
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-
+const firebaseUIConfig = {
+    signInOptions: [ //array of sign in options supported
+      //array can include just "Provider IDs", or objects with the IDs and options
+      GoogleAuthProvider.PROVIDER_ID,
+      { provider: EmailAuthProvider.PROVIDER_ID, requiredDisplayName: true },
+    ],
+    signInFlow: 'popup', //don't redirect to authenticate
+    credentialHelper: 'none', //don't show the email account chooser
+    callbacks: { //"lifecycle" callbacks
+      signInSuccessWithAuthResult: () => {
+        return false; //don't redirect after authentication
+      }
     }
+  }
+  
 
-    const handleClick = (event) => {
-        console.log("clicked");
-    }
+// function LoginForm() {
 
-    return (
-        <div className='form'>
-            {/*  action="login.php" method="POST" */}
-            <form className="login-form" onSubmit={handleSubmit}>
-                <div className="mb-3">
-                    <label htmlFor="email" className="form-label">Email address</label>
-                    <input type="email" className="form-control" placeholder="name@example.com" name="email" id="email" required></input>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="password" className="form-label">Password</label>
-                    <input type="password" className="form-control" name="password" id="password" required></input>
-                </div>
+//     const handleSubmit = (event) => {
+//         event.preventDefault();
 
-                <button className="submit btn tbn-primary" type="submit" onClick={handleClick}>Sign In</button>
+//     }
+
+//     const handleClick = (event) => {
+//         console.log("clicked");
+//     }
+
+//     return (
+//         <div className='form'>
+//             {/*  action="login.php" method="POST" */}
+//             <form className="login-form" onSubmit={handleSubmit}>
+//                 <div className="mb-3">
+//                     <label htmlFor="email" className="form-label">Email address</label>
+//                     <input type="email" className="form-control" placeholder="name@example.com" name="email" id="email" required></input>
+//                 </div>
+//                 <div className="mb-3">
+//                     <label htmlFor="password" className="form-label">Password</label>
+//                     <input type="password" className="form-control" name="password" id="password" required></input>
+//                 </div>
+
+//                 <button className="submit btn tbn-primary" type="submit" onClick={handleClick}>Sign In</button>
      
-            </form>
+//             </form>
            
-        </div>
-    )
+//         </div>
+//     )
 
-}
+// }
 
 export function LoginPage() {
 
@@ -40,7 +58,7 @@ export function LoginPage() {
         <div>
             <div className="login-form">
                 <h1>Login</h1>
-                <LoginForm />
+                <StyledFirebaseAuth firebaseAuth={getAuth()} uiConfig={firebaseUIConfig}/>
             </div>
             <p className='login-p'>Don't have an account? Sign up as a <Link to="/create-account">student</Link> or apply to be a <Link to="/mentors">mentor</Link></p>
         </div>
