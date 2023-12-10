d0c325cbbd2ff398556754128bbef078c436e55f
Lasya <lasya19@uw.edu>
2023-12-09 17:31:54 -0800

    merge conflicts

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -17,22 +17,49 @@ export function NavBar(props) {
         console.log("Signing Out");
         signOut(getAuth());
     }
-    
-   
 
-    return (
-        <header className='header'>
-            <div className='navbar-container'>
-                <NavLink to="/home" className="logo"><img src="img/smile.png" alt="husky to husky logo"></img></NavLink>
-                <div className="navbar-profile">
+    function signedOut() {
+        return (
+            <div className="navbar-profile">
+                <nav className="navbar">
+                    <NavLink to="/mentor-application">Apply</NavLink>
+                    <NavLink to="/mentors">Mentors</NavLink>
+                    {isLoggedIn ? (
+                        <NavLink to="/" onClick={handleSignOut}>Sign Out</NavLink>
+                    ) : (
+                        <NavLink to="/login">Login</NavLink>
+                    )}
+                </nav>
+
+                <div className="profile-container">
+                    <img src="img/profile-user.png" className="profile" alt="profile icon"></img>
+                    <div className="dropdown-menu navbar">
+                        <ul>
+                            <NavLink className="navbar-brand profile-nav" to="/profile">Profile</NavLink>
+                            <NavLink className="navbar-brand admin-nav" to="/mentor-approval">Admin</NavLink>
+                            {isLoggedIn ? (
+                                <li><NavLink className="navbar-brand login-nav" to="/" onClick={handleSignOut}>Sign Out</NavLink></li>
+                            ) : (
+                                <li><NavLink className="navbar-brand login-nav" to="/login" >Login</NavLink></li>
+                            )}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    function signedIn() {
+        return(
+            <div className="navbar-profile">
                     <nav className="navbar">
                         <NavLink to="/mentor-application">Apply</NavLink>
                         <NavLink to="/mentors">Mentors</NavLink>
                         {isLoggedIn ? (
-                                    <NavLink to="/" onClick={handleSignOut}>Sign Out</NavLink>
-                                ): (
-                                    <NavLink to="/login">Login</NavLink>
-                                )}
+                            <NavLink to="/" onClick={handleSignOut}>Sign Out</NavLink>
+                        ) : (
+                            <NavLink to="/login">Login</NavLink>
+                        )}
                     </nav>
 
                     <div className="hamburger-menu">
@@ -53,7 +80,57 @@ export function NavBar(props) {
                                 <NavLink className="navbar-brand admin-nav" to="/mentor-approval">Admin</NavLink>
                                 {isLoggedIn ? (
                                     <li><NavLink className="navbar-brand login-nav" to="/" onClick={handleSignOut}>Sign Out</NavLink></li>
-                                ): (
+                                ) : (
+                                    <li><NavLink className="navbar-brand login-nav" to="/login" >Login</NavLink></li>
+                                )}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+        )
+    }
+
+    // function interfaceChange((isLogged) =>{
+    //     if(isLogged){
+    //         {sign}
+    //     }
+    // })
+
+
+    return (
+        <header className='header'>
+            <div className='navbar-container'>
+                <NavLink to="/home" className="logo"><img src="img/smile.png" alt="husky to husky logo"></img></NavLink>
+                <div className="navbar-profile">
+                    <nav className="navbar">
+                        <NavLink to="/mentor-application">Apply</NavLink>
+                        <NavLink to="/mentors">Mentors</NavLink>
+                        {isLoggedIn ? (
+                            <NavLink to="/" onClick={handleSignOut}>Sign Out</NavLink>
+                        ) : (
+                            <NavLink to="/login">Login</NavLink>
+                        )}
+                    </nav>
+
+                    <div className="hamburger-menu">
+                        <img src="img/hamburger.png" alt="hamburger menu icon" className="hamburger"></img>
+                        <div className="dropdown-menu navbar">
+                            <ul>
+                                <li><NavLink className="navbar-brand apply-nav" to="/mentor-application">Apply</NavLink></li>
+                                <li><NavLink className="navbar-brand mentors-nav" to="/mentors">Mentors</NavLink></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="profile-container">
+                        <img src="img/profile-user.png" className="profile" alt="profile icon"></img>
+                        <div className="dropdown-menu navbar">
+                            <ul>
+                                <NavLink className="navbar-brand profile-nav" to="/profile">Profile</NavLink>
+                                <NavLink className="navbar-brand admin-nav" to="/mentor-approval">Admin</NavLink>
+                                {isLoggedIn ? (
+                                    <li><NavLink className="navbar-brand login-nav" to="/" onClick={handleSignOut}>Sign Out</NavLink></li>
+                                ) : (
                                     <li><NavLink className="navbar-brand login-nav" to="/login" >Login</NavLink></li>
                                 )}
                             </ul>
