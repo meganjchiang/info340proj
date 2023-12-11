ff68c6563dd050f4dd1d50f683dffcd3ce0aee04
meganjchiang <mjchiang@uw.edu>
2023-12-10 19:58:22 -0800

    coneect firebase data from mentor applications to admin approval table

--- src/components/AdminMentorApproval.js
+++ src/components/AdminMentorApproval.js
@@ -1,16 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
+import { getDatabase, ref, onValue } from 'firebase/database';
 
 export function ApproveAdmin(props) {
+    const [mentorStateArray, setMentorStateArray] = useState([]);
+    
+    useEffect(() => {
+      const db = getDatabase();
+      const allMentorRef = ref(db, "allMentor");
+
+      onValue(allMentorRef, function (snapshot) {
+          const allMentorsObj = snapshot.val();
+          const keyArray = Object.keys(allMentorsObj);
+          const allMentorsArray = keyArray.map((keyString) => {
+              const mentorObj = allMentorsObj[keyString];
+              mentorObj.firebasekey = keyString;
+              return mentorObj
+          })
+          console.log(allMentorsArray);
+          // //update the state
+          setMentorStateArray(allMentorsArray);
+          console.log(mentorStateArray);
+      })
+    }, [])
+
+
     //map here
-    const mentorApplication = props.appliedMentors;
-    const mentorRows = mentorApplication.map((mentor, index) => {
-        const returnTable = (
-            <tr key={mentor.netId}>
+    // const mentorApplication = props.appliedMentors;
+    // const mentorRows = mentorApplication.map((mentor, index) => {
+    //     return (
+    //         <tr key={mentor.netId}>
+    //         <th scope="row">{index + 1}</th>
+    //         <td>{mentor.firstName}</td>
+    //         <td>{mentor.lastName}</td>
+    //         <td>
+    //         <Button variant="success">Approve</Button>{' '}
+    //         <Button variant="danger">Decline</Button>{' '}
+    //         </td>
+    //         <td>
+    //             <Button variant="secondary">Transcript</Button>{' '}
+    //         </td>
+    //     </tr>
+    //     );
+    // })
+
+    const mentorRows = mentorStateArray.map((mentor, index) => {
+        return (
+            <tr key={mentor.firebasekey}>
             <th scope="row">{index + 1}</th>
-            <td>{mentor.firstName}</td>
-            <td>{mentor.lastName}</td>
+            <td>{mentor.first}</td>
+            <td>{mentor.lastn}</td>
             <td>
             <Button variant="success">Approve</Button>{' '}
             <Button variant="danger">Decline</Button>{' '}
@@ -19,9 +59,7 @@ export function ApproveAdmin(props) {
                 <Button variant="secondary">Transcript</Button>{' '}
             </td>
         </tr>
-        )
-        return returnTable;
-        
+        );
     })
 
 
@@ -29,6 +67,7 @@ export function ApproveAdmin(props) {
         <div>
             <h1 className="headingForMentor"> Mentor Applications </h1>
             <div className="table-style">
+                {/* {mentorStateArray} */}
             <Table striped="columns">
                 <thead className="thead-dark">
                     <tr>
