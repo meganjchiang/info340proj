42ae8f0e8b7a6da5641c0f306389c6eb36619da4
Lasya <lasya19@uw.edu>
2023-12-11 00:43:51 -0800

    approve and deny feature

--- src/components/AdminMentorApproval.js
+++ src/components/AdminMentorApproval.js
@@ -1,59 +1,76 @@
 import React, { useEffect, useState } from "react";
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
-import { getDatabase, ref, onValue, set as firebaseSet, push as firebasePush } from 'firebase/database';
+import { getDatabase, ref, onValue, push as firebasePush, remove } from 'firebase/database';
 
 export function ApproveAdmin(props) {
     const [mentorStateArray, setMentorStateArray] = useState([]);
-    
-    
-    useEffect(() => {
-      const db = getDatabase();
-      const allMentorRef = ref(db, "mentorApplicants");
+    const [isEmpty, setIsEmpty] = useState(false);
 
-      onValue(allMentorRef, function (snapshot) {
-          const allMentorsObj = snapshot.val();
-          const keyArray = Object.keys(allMentorsObj);
-          const allMentorsArray = keyArray.map((keyString) => {
-              const mentorObj = allMentorsObj[keyString];
-              mentorObj.firebasekey = keyString;
-              return mentorObj
-          })
-      
-          // //update the state
-          setMentorStateArray(allMentorsArray);
-      })
+    useEffect(() => {
+        const db = getDatabase();
+        const allMentorRef = ref(db, "mentorApplicants");
+
+        if (allMentorRef === null) {
+            setIsEmpty(true);
+        }
+        onValue(allMentorRef, function (snapshot) {
+            const allMentorsObj = snapshot.val();
+            if (allMentorsObj) {
+                const keyArray = Object.keys(allMentorsObj);
+                const allMentorsArray = keyArray.map((keyString) => {
+                    const mentorObj = allMentorsObj[keyString];
+                    mentorObj.firebasekey = keyString;
+                    return mentorObj
+                })
+                setMentorStateArray(allMentorsArray);
+            } else {
+                setIsEmpty(true);
+            }
+
+
+        })
     }, [])
-    
+
     const handleApprove = (userKey) => {
         const db = getDatabase();
-        const mentorRef = ref(db, 'mentorApplicants/'+userKey);
+        const mentorRef = ref(db, 'mentorApplicants/' + userKey);
         console.log(mentorRef);
-        onValue(mentorRef, function(snapshot){
+        onValue(mentorRef, function (snapshot) {
             const mentorObj = snapshot.val();
-            
             const allMentorsRef = ref(db, "allMentors")
-            firebasePush(allMentorsRef, mentorObj )
-        
+            firebasePush(allMentorsRef, mentorObj)
+            remove(mentorRef);
         })
-        console.log(userKey);
+
     }
+
+    const handleDecline = (userKey) => {
+        const db = getDatabase();
+        const mentorRef = ref(db, 'mentorApplicants/' + userKey);
+        remove(mentorRef);
+    }
+
+
 
 
     const mentorRows = mentorStateArray.map((mentor, index) => {
         return (
             <tr key={mentor.firebasekey}>
-            <th scope="row">{index + 1}</th>
-            <td>{mentor.first}</td>
-            <td>{mentor.lastn}</td>
-            <td>
-            <Button variant="success" onClick={()=> handleApprove(mentor.firebasekey)}>Approve</Button>{' '}
-            <Button variant="danger">Decline</Button>{' '}
-            </td>
-            <td>
-                <Button variant="secondary">Transcript</Button>{' '}
-            </td>
-        </tr>
+                <th scope="row">{index + 1}</th>
+                <td>{mentor.first}</td>
+                <td>{mentor.lastn}</td>
+                <td>
+                    <Button variant="success" onClick={() => handleApprove(mentor.firebasekey)}>Approve</Button>{' '}
+                    <Button variant="danger" onClick={() => handleDecline(mentor.firebasekey)}>Decline</Button>{' '}
+                </td>
+                <td>
+                    <Button variant="secondary">Transcript</Button>{' '}
+                </td>
+            </tr>
+
+
+
         );
     })
 
@@ -61,23 +78,25 @@ export function ApproveAdmin(props) {
     return (
         <div>
             <h1 className="headingForMentor"> Mentor Applications </h1>
+
             <div className="table-style">
-                {/* {mentorStateArray} */}
-            <Table striped="columns">
-                <thead className="thead-dark">
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Approve/Deny</th>
-                        <th scope="col">Transcript</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {mentorRows}
-                </tbody>
-            </Table>
-            </div>
-        </div>
+                <Table striped="columns">
+                    <thead className="thead-dark">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Approve/Deny</th>
+                            <th scope="col">Transcript</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {mentorRows}
+                    </tbody>
+                </Table>
+            </div >
+
+        </div >
     )
 }
